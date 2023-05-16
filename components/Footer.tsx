5c187d8b029430a399b8d34ce11c01b67ce95d5c
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-05-15 21:20:06 -0500

    Add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='mt-3 text-center'> 
+      <div>
+        <p>developed by Joaquin</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
