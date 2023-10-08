637a025dd7326d8f65b5d80e33919f10e4d70c71
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-10-08 18:58:31 -0500

    Add components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,21 @@
 import Layout from "../../components/Layout";
+import Hero from "../../components/Hero";
+import About from "../../containers/About";
+
 const Home = () => {
   return (
-    <Layout>
-      <div className="flex flex-col items-center justify-center w-full h-full">
-        <div className="bg-hero-pattern h-96 w-full bg-no-repeat bg-size-cover">
-          Hola
+    <>
+      <Hero />
+      <Layout>
+        <div className="absolute top-20 left-0 z-10 pt-10 pl-4">
+          <h1 className="font-medium text-xl text-white">
+            Hola, soy Joaquin de Jesus Cruz Bravo.
+          </h1>
+          <p className="text-base text-white">Desarrollador web frontend.</p>
         </div>
-        <h1 className="font-medium text-xl">Hola, soy Joaquin de Jesus Cruz Bravo.</h1>
-        <p className="text-lg">Soy desarrollador web frontend.</p>
-      </div>
-    </Layout>
+        <About/>
+      </Layout>
+    </>
   );
 };
 
