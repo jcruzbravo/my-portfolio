7f1c9fda8a9efe199e6b9b3540b151715691a9f2
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-10-08 18:56:00 -0500

    Add breakpoints to media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': {
+        'max': '768px',
+      },
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       backgroundImage: {
         'hero-pattern': "url('src/assets/images/hero.jpg')",
