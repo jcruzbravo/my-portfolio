080d3b329d241df9d64ad58bac511d77a025f373
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-10-08 18:56:54 -0500

    Add data

---
+++ src/data/index.js
@@ -0,0 +1,63 @@
+import html from "/src/assets/images/html.png";
+import css from "/src/assets/images/css.png";
+import angular from "/src/assets/images/angular.png";
+import bootstrap from "/src/assets/images/bootstrap.png";
+import javascript from "/src/assets/images/js.png";
+import nextjs from "/src/assets/images/next.svg";
+import react from "/src/assets/images/react.png";
+import tailwind from "/src/assets/images/tailwind.png";
+import typescript from "/src/assets/images/typescript.png";
+import sass from "/src/assets/images/sass.png";
+import vercel from "/src/assets/images/vercel.svg";
+import cake from "/src/assets/images/cakephp.jpg";
+import csharp from "/src/assets/images/csharp.png";
+import laravel from "/src/assets/images/laravel.png";
+import expressjs from "/src/assets/images/express.png";
+import nodejs from "/src/assets/images/nodejs.png";
+import php from "/src/assets/images/php.png";
+import mysql from "/src/assets/images/mysql.png";
+import postgresql from "/src/assets/images/postgresql.png";
+import sqlserver from "/src/assets/images/sqlserver.svg";
+import git from "/src/assets/images/git.png";
+import github from "/src/assets/images/github.svg";
+import gitlab from "/src/assets/images/gitlab.png";
+import apirest from "/src/assets/images/apirest.png";
+import npm from "/src/assets/images/npm.png";
+
+
+export const frontEndSkills = [
+  { name: "HTML", image: html },
+  { name: "CSS", image: css },
+  { name: "Angular", image: angular },
+  { name: "Bootstrap", image: bootstrap },
+  { name: "JavaScript", image: javascript },
+  { name: "Next.js", image: nextjs },
+  { name: "React", image: react },
+  { name: "Tailwind CSS", image: tailwind },
+  { name: "TypeScript", image: typescript },
+  { name: "Sass", image: sass },
+  { name: "Vercel", image: vercel },
+];
+
+export const backEndSkills = [
+  { name: "CakePhp", image: cake },
+  { name: "Laravel", image: laravel },
+  { name: "C#", image: csharp },
+  { name: "ExpressJs", image: expressjs },
+  { name: "NodeJs", image: nodejs },
+  { name: "PHP", image: php },
+];
+
+export const databaseSkills = [
+  { name: "MySQL", image: mysql },
+  { name: "PostgreSQL", image: postgresql },
+  { name: "SQL Server", image: sqlserver },
+];
+
+export const toolsSkills = [
+  { name: "Git", image: git },
+  { name: "Github", image: github },
+  { name: "Gitlab", image: gitlab },
+  { name: "ApiRest", image: apirest },
+  { name: "NPM", image: npm },
+]
