6523ae8dc7cf0552e6ab57bd1491a8c47e53bb5d
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-10-14 11:38:00 -0500

    Remove opacity

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="bg-hero-pattern md:bg-left h-96 bg-no-repeat bg-size-cover opacity-90"></div>
+    <div className="bg-hero-pattern md:bg-left h-96 bg-no-repeat bg-size-cover"></div>
   );
 };
 
