fa0a3a5c2e680087f090b6cecce102a38793d4c3
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-10-15 08:13:28 -0500

    Add version and latest update

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -59,7 +59,8 @@ function Footer() {
             </a>
           </div>
           <div className="text-center text-white">
-            <p className="text-sm">&copy; 2023 JoacoDev | Versión 1.0</p>
+            <p className="text-sm">&copy; 2023 JoacoDev</p>
+            <span className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300 text-center w-full">Versión 1.0 | Ultima actualización 15 de octubre de 2023</span>
           </div>
         </div>
       </div>
