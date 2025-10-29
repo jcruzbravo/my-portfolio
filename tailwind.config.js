927975575ff232a8d801de18d353a9cb80fdcf5b
jcruzbravo2000 <jcruzbravo2000@gmail.com>
2025-10-29 13:03:47 -0500

    feat: update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      'sm': {
-        'max': '640px',
-      },
-      'md': {
-        'max': '768px',
-      },
-      'lg': {
-        'max': '1024px',
-      },
-      'xl': '1280px',
-      '2xl': '1536px',
+      sm: { max: "640px" },
+      md: { max: "768px" },
+      lg: { max: "1024px" },
+      xl: "1280px",
+      "2xl": "1536px",
     },
-    extend: {
-      backgroundImage: {
-        'hero-pattern': "url('https://res.cloudinary.com/dubztfhgv/image/upload/v1697405826/hero_ji8vd9.jpg')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
